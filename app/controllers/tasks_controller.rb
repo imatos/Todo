406426a317a39c45ef36f0889e8737a836d91073
Iuri Matos <iuriamatos@gmail.com>
2015-12-16 23:41:56 -0200

    added a user reference to tasks.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,9 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks
   end
 
   def new
@@ -13,7 +14,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.new task_params
     if @task.save
       redirect_to tasks_path, notice: 'Task was successfully created.'
     else
